41e80bfac57297876bd4d23223e4847538e99359
AlexCbk <togoq@icloud.com>
2025-04-07 20:58:47 +0300

    Update models.py

--- blogicum/core/models.py
+++ blogicum/core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class PublishedCreatedModel(models.Model):
     """Абстрактная модель. Добавляет флаг is_published."""
+
     is_published = models.BooleanField(default=True,
                                        verbose_name='Опубликовано',
                                        help_text='Снимите галочку, чтобы '
